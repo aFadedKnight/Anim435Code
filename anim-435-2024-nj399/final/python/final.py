898cd37054cbec6299c4c6ec6275552147b45ddd
Nicky J. <nickyj0402@gmail.com>
2024-11-27 09:58:15 -0500

    Updated checks and commented out code to allow easier testing but ignores checks

--- anim-435-2024-nj399/final/python/final.py
+++ anim-435-2024-nj399/final/python/final.py
@@ -4,26 +4,31 @@
 import argparse
 import json, csv, os
 
-def decimate(amount=50, file=""):
+def decimate(amount=50, file="", directory=""):
         """
         This function uses amount and will decimate geometry from a file based on the amount passed in.
 
         Args:
         amount = value to decimate gemoetry by
+        file = name of the file that will be decimated
+        directory = path to the file that will be decimated and where the new file will save
 
         Returns:
         None
         """
-        print(amount)
-        print(file)
+        print(amount,file,directory)
+
+        # TO-DO: get information on original polycount
+
         # TO-DO: create code that will decimate geometry inside a given file.
 
         # Save File
         #maya.cmds.file(rename=r"new name in same directory")
         #maya.cmds.file(save=True)
 
-        # Export CSV
-        csv_file = f'{og_file_name}.{amount}.report.csv'
+        # Export CSV (needs variables in the data variable to be defined - geometry, og_polycount, new_polycount)
+        '''
+        csv_file = f'{directory}/{file}.{amount}.report.csv'
 
         data = [
             ['Geometry', 'OG Polycount', "New Polycount", "Amount Removed"],  # Headers
@@ -33,6 +38,7 @@ def decimate(amount=50, file=""):
         with open(csv_file, mode='w', newline='', encoding='utf-8') as file:
             writer = csv.writer(file)
             writer.writerows(data)
+        '''
 
  
 def main():
@@ -46,17 +52,20 @@ def main():
     file_to_decimate = args.file
 
     amount="half"
-    file_to_decimate="C:\\Users\\nicky\\Desktop"
+    file_to_decimate="C:/Projects/file.ma"
 
-    # Check file location
-    if not os.path.exists(file_to_decimate):
-         print('Path to file does not exist')
+    # Check file exists and is a maya file
+    #if not os.path.exists(file_to_decimate):
+    #    print('Path to file does not exist')
+    #elif not file_to_decimate.endswith('.mb','.ma'):
+    #     print('Path given was not a maya file')
+
 
     # Check if amount is an integer value if not get data from json
     try:
           amount = int(amount)
     except:
-        lods = "..\\config\\lods.json"
+        lods = "final/config/lods.json"
         try:
             with open(lods, 'r') as file:
                 lod_values = json.load(file)
@@ -71,8 +80,12 @@ def main():
                 print("Unable to open JSON file")
                 exit()
 
+    directory = os.path.dirname(file_to_decimate)
+    file_to_decimate = os.path.basename(file_to_decimate)
+
+    print(directory)
 
     # Decimate File
-    decimate(amount,file_to_decimate)
+    decimate(amount,file_to_decimate,directory)
 
 main()
