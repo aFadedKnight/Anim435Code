45e875bf9d2a711ddb93bc3aa1a9d8621e2a8f23
Nicky J. <nickyj0402@gmail.com>
2024-12-07 19:17:53 -0500

    Logger updates and added mode argument

--- anim-435-2024-nj399/final/README.md
+++ anim-435-2024-nj399/final/README.md
@@ -6,6 +6,8 @@ Decimate geometry inside a maya file to create different LODs
 #### Command Prompt
 Open a command prompt. You can then run the script using '"C:\Program Files\Autodesk\Maya2025\bin\mayapy.exe" "final.py" -f "c:/path/to/maya/file" -a half'. Make sure that you are using the path to your mayapy.exe as well as the path to your final.py file. Most likely the path used in the example will be where your mayapy is if using maya 2025(just change the 2025 to be your correct verison if not using 2025)
 
+You can also pass through a mode argument when running the script using -m or --mode.
+
 #### Git Bash
 In the aliases.sh folder, genereall located at "C:\Program Files\Git\etc\profile.d\aliases.sh", paste in the following alias which will make it so you don't need type the entire path to mayapy evertime.
 ```bash
@@ -16,6 +18,8 @@ Most likely the path used in the example will be where your mayapy is if using m
 
 Open a Git Bash window. You can then run the script using 'mayapy "c:/path/to/final.py" -f "c:/path/to/maya/file" -a half'. Make sure that you are using the path to your final.py file.
 
+You can also pass through a mode argument when running the script using -m or --mode.
+
 ### Arguments
 
 #### file
@@ -25,6 +29,7 @@ Give the path to the maya file that you want to have decimated. This is also the
 
 #### amount
 --amount or - a
+
 Specify the amount that you want to decimate a file by. You can use the predertimined values or use a specific percentage that you want to decimate the model by.
 
 Predeetrmined values:
@@ -35,3 +40,14 @@ Predeetrmined values:
 "third": 33
 "quarter": 25
 "eigth": 13
+
+#### mode
+--mode or -m
+
+Decide what mode you want to use to reduce your geometry. by default the preserve mode is used
+
+**"preserve**
+Using the preserve mode will be less accurate in reaching the desired reduction amount entered For example if you are reducing by 50 percent if there are 100 faces it may only reduce to 60+ faces. This methos will do a better job at preserving detail as well as keeping quads which better keeps the shape of your object. For instance a cube would remain a cube with likely 0 overlapping
+
+**"decimate"**
+Using the decimate mode will more accuratley reach the desired the reduction amount entered. For example if you are reducing by 50 percent if there are 100 faces it will reduce to almost if not exacly 50 faces. However this could lose more detail in the geometry and remove faces in an unwanted way such as breaking up hard edges. For instance a cube may no longer have all of it's hard edges and some geometry may overlap.
--- anim-435-2024-nj399/final/python/final.py
+++ anim-435-2024-nj399/final/python/final.py
@@ -1,11 +1,20 @@
 import maya.standalone
 import maya.cmds as cmds
-import argparse
-import json, csv, os
+from datetime import datetime
+import json, csv, os, argparse, logging
 
+# Start mayapy
 maya.standalone.initialize(name="python")
 
-def decimate(amount=50, file="", directory=""):
+#Setup Logger
+logger = logging.getLogger(__name__)
+
+FORMAT = "[%(asctime)s][%(filename)s][%(levelname)s] %(msg)s"
+
+logging.basicConfig(filename='log.txt',level=logging.INFO, format=FORMAT)
+logger.info('Starting')
+
+def decimate(amount=50, file="", directory="", mode='preserve'):
         """
         This function uses amount and will decimate geometry from a file based on the amount passed in. 
         It will finish by saving out a new maya binary file with the decimated geometry and a csv file containing some metadata.
@@ -14,20 +23,27 @@ def decimate(amount=50, file="", directory=""):
         amount = value to decimate gemoetry by
         file = name of the file that will be decimated
         directory = path to the file that will be decimated and where the new file will save
+        mode = determines settings to use for reducing geometry
 
         Returns:
         None
         """
+        logger.info("Begin geometry reduction")
         # Open the Maya file
         maya_file = f'{directory}/{file}'
         cmds.file(maya_file, open=True, force=True)
 
+        logger.info("Iterating through geo in file")
+        logger.warning(f'Using {mode} mode to reduce geometry')
         # Iterate through geometry and reduce polycount
         all_meshes = cmds.ls(type="mesh")
         og_polycount = 0
         for mesh in all_meshes:
             og_polycount += cmds.polyEvaluate(mesh, face=True)
-            cmds.polyReduce(mesh,p=amount,kqw=0.5, version=1)
+            if mode == 'preserve':
+                cmds.polyReduce(mesh,p=amount,kqw=0.5,version=1)
+            else: 
+                cmds.polyReduce(mesh,p=amount)
 
         # Generate output filename
         output_file = (f"{maya_file}.{amount}.mb")
@@ -35,8 +51,9 @@ def decimate(amount=50, file="", directory=""):
         # Save the decimated file
         cmds.file(rename=output_file)
         cmds.file(save=True, type="mayaBinary")
-        print(f"Decimated file saved as: {output_file}")
+        logger.info(f"Decimated file saved as: {output_file}")
 
+        logger.info('Generating CSV metadata')
         # Iterate through geometry and get polycount
         cmds.file(output_file, open=True, force=True)
         all_meshes = cmds.ls(type="mesh")
@@ -48,39 +65,52 @@ def decimate(amount=50, file="", directory=""):
         csv_file = f'{directory}/{file}.{amount}.report.csv'
 
         data = [
-            ['File', 'OG Polycount', "New Polycount", "Percent Removed"],  # Headers
-            [file, og_polycount, new_polycount, amount]
+            ['File', 'OG Polycount', "New Polycount", "Percent Removed", "Log Date/Time"],  # Headers
+            [file, og_polycount, new_polycount, amount, datetime.now().strftime("%Y-%m-%d %H:%M:%S")]
         ]
         
         with open(csv_file, mode='w', newline='', encoding='utf-8') as file:
             writer = csv.writer(file)
             writer.writerows(data)
+            logger.warning(f"CSV file exported: {csv_file}")
 
  
 def main():
+    """
+        This function sets up the argparse arguments and processes them to be passed through to the decimate() function
+
+        Args:
+        None
+
+        Returns:
+        None
+        """
     # Set up argparse for command-line arguments
     parser = argparse.ArgumentParser(description="Decimate geometry inside a file by a given amount")
     parser.add_argument('-a','--amount', type=str, default="half", help='Amount to decimate geometry. Can be string(half, quarter, two-thirds, third, eitgh, three-quarters) or specific integer value')
     parser.add_argument('-f','--file', type=str, help='Path to file that you want to decimate.')
+    parser.add_argument('-m','--mode', type=str, help='Mode to decimate by, can be either the "decimate" which is more accurate to the amount you want to reduce geometry by but can mess up geo or the default "preserve" which preserves the geometry better but does not reduce as accurately.')
 
     args = parser.parse_args()
     amount = args.amount
     file_to_decimate = args.file
-
-    #amount="half"
-    #file_to_decimate="C:/Projects/file.ma"
+    mode =args.mode
 
     # Check file exists and is a maya file
     if not os.path.exists(file_to_decimate):
-        print('Path to file does not exist')
+        logger.error(f"Path to file does not exsist: {file_to_decimate}")
+        exit()
     elif not file_to_decimate.endswith('.ma') and not file_to_decimate.endswith('.mb'):
-         print('Path given was not a maya file')
+         logger.error(f"File given is not a maya file: {file_to_decimate}")
+         exit()
 
 
     # Check if amount is an integer value if not get data from json
     try:
+          logger.info("Chacking amount value")
           amount = int(amount)
     except:
+        logger.info("Getting amount setting from lods.json")
         lods = "C:\\Users\\nicky\\OneDrive\\Documents\\GitHub\\Anim435Code\\anim-435-2024-nj399\\final\\config\\lods.json" # Change this to path of your json file
         try:
             with open(lods, 'r') as file:
@@ -89,19 +119,17 @@ def main():
             try:
                  amount = lod_values[amount]
             except:
-                 print(f'Amount "{amount}" not recognized')
+                 logger.error(f'Amount "{amount}" is not recognized')
                  exit()
             
         except Exception as e:
-                print("Unable to open JSON file")
+                logger.error(f"Unable to access lods.json file, can't load settings:\n{lods}\n{e}")
                 exit()
 
     directory = os.path.dirname(file_to_decimate)
     file_to_decimate = os.path.basename(file_to_decimate)
 
-    print(directory)
-
     # Decimate File
-    decimate(amount,file_to_decimate,directory)
+    decimate(amount,file_to_decimate,directory, mode)
 
 main()
